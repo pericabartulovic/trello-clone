9c7bf130cf5c3e1c415b3b6fe319a7a8ac0668ec
pericabartulovic <pericab35@gmail.com>
2024-12-05 17:38:24 +0100

    url to prod, styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true, // Set to true for production
   //   apiUrl: 'https://dpg-ct2ug92j1k6c73b18p1g-a.frankfurt-postgres.render.com',
-  apiUrl: 'http://localhost:8080/api',
-};
+//   apiUrl: 'http://localhost:8080/api', //local
+  apiUrl: 'https://trello-clone-backend-2izk.onrender.com/api', //prod
+}; 
